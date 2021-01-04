e6d39ba53256bbdb3ae08ded10637c59af81b4f0
Shraddha Gupta <69422250+shraddha-1402@users.noreply.github.com>
2021-01-04 23:33:43 +0530

    Updated temp.js

--- src/components/temp.js
+++ src/components/temp.js
@@ -1,21 +1,27 @@
-const Temp = ({songName, lyrics, image}) => {
-  return(
-    
-      <div className="temp-card">        
+
+// import photo1 from '../images/card-image.jpg'
+import React from "react";
+// import Overlay from "./overlay"
+
+const Temp = ({ songName, lyrics, image }) => {
+  return (
+    <React.Fragment>
+      {/* <Overlay /> */}
+      <div className="temp-card">
         <div className="box">
           <div className="imgBx">
-            <img src={image} alt="taehyung's"/>
+            <img src={image} alt="taehyung's" />
           </div>
           <div className="contentBx">
             <div>
-              <p>
-                {lyrics}  
-              </p>
+              <p>{lyrics}</p>
               <h5>{songName}</h5>
             </div>
           </div>
-        </div>      
+        </div>
       </div>
-  )
-}
+    </React.Fragment>
+  );
+};
+
 export default Temp;
