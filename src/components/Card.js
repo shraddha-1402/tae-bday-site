9a3b7a7a6effb88bfb6ab2c5a53ea2f4a8831e30
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-01-09 16:37:03 +0530

    some not required changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Card = ({ key, songName, lyrics, image }) => {
-  // const music = new Audio(`${window.location.origin}/bdaySong.mp3`);
+  const music = new Audio(`${window.location.origin}/bdaySong.mp3`);
   return (
     <div key={key} className="temp-card">
       <div
         className="box"
-        // onMouseEnter={() => {
-        //   music.play();
-        // }}
-        // onMouseLeave={() => {
-        //   music.pause();
-        // }}
+        onMouseEnter={() => {
+          music.play();
+        }}
+        onMouseLeave={() => {
+          music.pause();
+        }}
       >
         <div className="imgBx">
           <img src={image} alt="taehyung's" />
